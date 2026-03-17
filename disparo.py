ca60598aa9f9f37b84f085012c52e6d729fb774c
arseoane <arodriguezseoane@danielcastelao.org>
2026-03-17 13:53:17 +0100

    disparo.py

---
+++ disparo.py
@@ -0,0 +1,8 @@
+class Disparo:
+    def __init__(self, fila, columna):
+        self.fila = fila
+        self.columna = columna
+        self.resultado = ""
+
+    def procesar_disparo(self, tablero):
+        return ""
